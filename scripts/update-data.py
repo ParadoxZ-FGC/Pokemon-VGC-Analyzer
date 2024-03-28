499f7fd32a08fdc09521f0f9c3b1b1cf79d5b0af
Noah (paradox) <163929425+ParadoxZ-FGC@users.noreply.github.com>
2024-03-28 09:12:04 -0500

    Update update-data.py

--- scripts/update-data.py
+++ scripts/update-data.py
@@ -1,6 +1,12 @@
 # Crawl sites : Gather top sets/pick rates
 
+import requests  # library that allows reading web pages
 
+regulation = "f"
+root_urls = ["https://victoryroadvgc.com/2024-season-calendar/", "https://www.smogon.com/dex/sv/formats/vgc24-regulation-{}/".format(regulation)]
+
+vr_base = requests.get(root_urls[0])          # Victory Road 2024 Season Calendar page object
+smogon_reg_base = requests.get(root_urls[1])  # Smogon current Regulation page object
 
 # search site(s)
 
@@ -8,5 +14,4 @@
 
 # write pick rates (maybe sets) to file
 
-
-
+print(vr_base.text)
